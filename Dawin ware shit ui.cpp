af5e0e86835e05366d6c7e777bc4c90e931c1a11
SCOPES-1337 <75390603+XINDK@users.noreply.github.com>
2021-02-26 18:02:42 +0100

    Add files via upload

---
+++ Dawin ware shit ui.cpp
@@ -0,0 +1,40 @@
+void ToggleButton(const char* str_id, bool* v)
+{ 
+ImVec2 p = ImGui::GetCursorScreenPos(); 
+ImDrawList* draw_list = ImGui::GetWindowDrawList();
+float height = ImGui::GetFrameHeight();
+float width = height * 1.55f;
+float radius = height * 0.50f;
+ if (ImGui::InvisibleButton(str_id, ImVec2(width, height)))
+   *v = !*v;
+ImU32 col_bg;
+if (ImGui::IsItemHovered())
+	  col_bg = *v ? IM_COL32(145 + 20, 211, 68 + 20, 255) : IM_COL32(218 - 20, 218 - 20, 218 - 20, 255);
+ else
+	   col_bg = *v ? IM_COL32(145, 211, 68, 255) : IM_COL32(218, 218, 218, 255);
+  draw_list->AddRectFilled(p, ImVec2(p.x + width, p.y + height), col_bg, height * 0.5f);
+   draw_list->AddCircleFilled(ImVec2(*v ? (p.x + width - radius) : (p.x + radius), p.y + radius), radius - 1.5f, IM_COL32(255, 255, 255, 255));
+}
+
+
+void darwinwareshitui()
+{
+ImGui::SetNextWindowSize({452.f,276.f});
+
+ImGui::Begin( "DarinWare I Csgo Cheat ");
+ImGui::SetCursorPos({8.f,59.f});
+ImGui::Checkbox("GLOW", &the_bool);
+ImGui::SetCursorPos({8.f,83.f});
+ImGui::Checkbox("Aimbot", &the_bool);
+ImGui::SetCursorPos({8.f,106.f});
+ImGui::Checkbox("Bunnyhop", &the_bool);
+ImGui::SetCursorPos({8.f,35.f});
+ImGui::Checkbox("ESP", &the_bool);
+ImGui::SetCursorPos({5.f,134.f});
+ImGui::SliderInt("Fov changer", &valueI,0,100);
+
+
+ImGui::End();
+}
+
+
